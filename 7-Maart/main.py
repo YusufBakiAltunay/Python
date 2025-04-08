764eb67c8e04cc13beca831653486511600edc52
YusufBakiAltunay <9025526@student.zadkine.nl>
2025-04-08 10:18:25 +0200

    7 Maart

---
+++ 7-Maart/main.py
@@ -0,0 +1,79 @@
+# Variables (string, integer, float, boolean)
+
+#String
+
+first_name = "Yusuf"
+food = "pizza"
+email = "yusuf@gmail.com"
+
+#integer
+
+age = 25
+quantity = 3
+num_of_students = 30
+
+#float
+
+price = 19,99
+distance = 5,5
+
+#boolean
+
+is_student = True
+for_sale = False
+
+# String Methods(len(), upper(), lower(), find())
+
+# name2 = input("Enter your name: ")
+
+# result = len(name2)
+# result = name.find("y")
+# name2 = name2.upper()
+# name2 = name2.lower()
+
+
+
+
+#Typecasting(str(), int(), float(), bool())
+
+name1 = "Yusuf"
+age1 = 20 
+gpa = 3.2
+is_student = True
+
+age1 = str(age1)
+
+name1 = bool(name1)
+
+
+# Input()
+
+# name = input("What is your name?: ")
+# age = input("How old are you?: ")
+
+
+#Exercise Rectangle Area Cal
+
+# length = float(input("Enter the length: "))
+# width = float(input("Enter the width: "))
+# area = length * width 
+# print (f"The area is: {area}㎠")
+
+
+# Arithmetic 
+
+
+friends = 5 
+
+# friends = friends + 1
+# friends += 1
+# friends = friends - 2
+# friends -= 2
+# friends = friends * 3
+# friends *= 3
+# friends = friends / 5
+# friends /= 5
+# friends = friends ** 2
+# friends **= 2
+# friends = friends % 2
+# friends %= 2
