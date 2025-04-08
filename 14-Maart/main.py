51c632480fb9c86eb098651b4f041cf40eccf596
YusufBakiAltunay <9025526@student.zadkine.nl>
2025-04-08 10:38:21 +0200

    14 Maart

---
+++ 14-Maart/main.py
@@ -0,0 +1,27 @@
+# data-structuren
+
+# Lijsten
+
+# fruits = ["appel", "banaan", "kers"]
+# print(fruits[0])        # "appel"
+# fruits.append("druif")  # Voegt "druif" toe aan het einde
+# fruits.remove("banaan") # Verwijdert "banaan"
+
+#Tuples
+
+# point = (10, 20)
+# print(point[0])   10
+# point[0] = 30  # Dit geeft een foutmelding!
+
+#Sets
+
+# my_set = {1, 2, 3, 3}
+# print(my_set)  # {1, 2, 3}
+# my_set.add(4)
+
+#Dictionaries
+
+# persoon = {"naam": "Anka", "leeftijd": 25}
+# print(persoon["naam"])  # "Anka"
+# persoon["leeftijd"] = 26
+# persoon["land"] = "Nederland"
