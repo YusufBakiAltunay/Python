1670306f843c7ce2ca2572a8ffc182748a838aee
YusufBakiAltunay <9025526@student.zadkine.nl>
2025-04-15 14:36:53 +0200

    4 april

---
+++ 4-April/main.py
@@ -0,0 +1,48 @@
+# Conditionele Logica en Lussen(If-Else, For-loops, While-loops)
+
+
+#if_else
+# age = int(input("Enter ypur age:"))
+
+# if age >100:
+    
+#      print("You are too old to sign up")
+# elif age >= 18:
+#       print("You are now signed up")
+# elif age < 0:
+#       print("You have not been born yet")
+
+# else:
+    
+#     print("You must be 18+ to sign up")  
+
+
+
+
+#While-loops
+
+# num = int(input("Enter a # between 1 - 10: "))  
+
+# while num < 1 or num > 10:
+#      print(f"{num} is not valid")
+#      num = int(input("Enter a # between 1 - 10: "))
+
+# print(f"Your number is {num}") 
+
+
+
+
+#For_loops
+
+
+#  for x in reversed(range(1, 11)):
+#      print(x)
+    
+# print("happy new year")
+
+
+# for y in range(1, 11):
+#     if y == 5:
+#         continue
+#     else:
+#          print(y)
